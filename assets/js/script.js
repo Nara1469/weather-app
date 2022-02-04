12bee3f777703a68c5db5a8ac8d9f13d290d6e6e
Nara Davaasuren <naraamtm@gmail.com>
2022-02-04 09:26:54 -0700

    App appearence, style and script added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-const requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
+const myApiKey = 'b9d312a1f35b1b477f63e4d5e699509c';
+const cityName = 'Denver';
+
+const today = moment();
+
+$("#day1-date").text(today.format("l"));
+
+const searchBtn = document.getElementById('search-button');
+
+function getApi() {
+
+    const weatherUrl = 'api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + myApiKey;
+    console.log(weatherUrl);
+  
+    fetch(weatherUrl)
+      .then(function (response) {
+          console.log(response);
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        //   var listItem = document.createElement('li');
+        //   listItem.textContent = data[i].html_url;
+        //   repoList.appendChild(listItem);
+      });
+  }
+
+searchBtn.addEventListener('click', getApi);
